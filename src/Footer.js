253287cd4db59d95b630271a15ea7c28b50a272e
SuzetteSa <89647867+SuzetteSa@users.noreply.github.com>
2022-04-24 03:00:18 -0500

    update footer link

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="Footer">
-      <a href="https://github.com/SuzetteSa/weather-react">Open source code</a>{" "}
+      <a href="https://github.com/SuzetteSa/weather-react-app">
+        Open source code
+      </a>{" "}
       <span>by Suzette Samm</span>
     </div>
   );
